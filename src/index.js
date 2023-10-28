7b8fed1f8614ae00ccbeb92552e30933c23d0146
Thomas J McLeish <tjm@tjmcleish.com>
2023-10-28 10:12:37 -0700

    Added verbose comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import http from "http";
-import { config } from "dotenv";
-import app from "./app.js";
-import * as logger from "./utils/logger.js";
+// Import necessary modules from their respective packages.
+import http from "http";             // Native Node module to create HTTP server
+import { config } from "dotenv";     // Module to load environment variables from .env file
+import app from "./app.js";          // Import the Express app configuration
+import * as logger from "./utils/logger.js";  // Logger utility for consistent logging
 
+// Check if the application is running in a non-production environment
 if (process.env.NODE_ENV !== "production") {
+  // If it's a development or other non-production environment, load environment variables from .env file
   config();
 }
+
+// Create an HTTP server instance using the imported Express app.
+// The server will handle incoming requests and forward them to the Express app.
 const server = http.createServer(app);
 
+// Set the server port. Prioritize the PORT value from environment variables (useful for deployment platforms like Railway),
+// otherwise fall back to 3003 as the default port.
 const PORT = process.env.PORT || 3003;
 
+// Start the server to listen on the specified port.
+// Once the server starts successfully, it logs the listening port using the logger utility.
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
